927297e13796b33e8aded856a7ca56c3d264c650
Bernardo Caputo <bernardocaputo@gmail.com>
2017-12-19 00:10:42 -0200

    add auth plug in router

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -7,6 +7,7 @@ defmodule Hello.Auth do
 
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
-    user = user_id && repo.get(Rumbl.User, user_id) assign(conn, :current_user, user)
+    user = user_id && repo.get(Hello.User, user_id)
+    assign(conn, :current_user, user)
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Hello.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Hello.Auth, repo: Hello.Repo
   end
 
   pipeline :api do
